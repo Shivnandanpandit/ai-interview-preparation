12690101a34bd9831ee60f751e4aa2fb117fb80c
Shivnandanpandit <shivnandan959@gmail.com>
2026-08-02 15:59:07 +0530

    fix production auth and backend url

--- Frontend/src/features/interview/services/interview.api.js
+++ Frontend/src/features/interview/services/interview.api.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
     baseURL: 'https://ai-interview-preparation-sqyd.onrender.com',
+    // baseURL:"http://localhost:3000",
     withCredentials: true,
 })
 
